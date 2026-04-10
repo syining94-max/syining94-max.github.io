72f54a6948eacef2118024e36393d3e36a7f776f
syining94-max <syining94@gmail.com>
2026-04-10 02:05:22 +0000

    Deploying to gh-pages from @ syining94-max/syining94-max.github.io@1e48312ebe8356ef8c00a4037898edfe9abd289e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,21 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/al-folio/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/al-folio/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/al-folio/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
